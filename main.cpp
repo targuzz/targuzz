acf4a8b507af6910eed6dde9b51de352a5a72e2f
targuzz <87738147+targuzz@users.noreply.github.com>
2021-07-21 19:04:00 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
-#include <iosteam>
-using namespace std;
+#include "include/list.h"
 
-int main() {
-    std::cout << "Hello World!!!" << std::endl;
-    //putchar('/');
+int main(int arg_count, char *args[]) {
+    if( arg_count > 1 ) {
+        List Welcome;
+        Welcome.name = string(args[1]);
+        Welcome.print_menu();
+    }
+    else {
+        cout << "Usernane not supplied.. exiting the program" << endl; 
+    }
     return 0;
- }
+}
+
